438ede73cfe1520fd3d0b48ff302493aa80929b2
Priya Dugg <thepriyaduggal@gmail.com>
2024-06-26 22:21:37 +0000

    commited by Priya

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+jobs =[
+  {'title': 'Python Developer', 'location': 'Remote', 'salary': '100k'},
+  {'title': 'Data Analyst', 'location': 'Remote', 'salary': '120k'},
+  {'title': 'Data Scientist', 'location': 'Remote', 'salary': '150k'},
+  {'title': 'Frontend Developer', 'location': 'Remote', 'salary': '80k'},
+  {'title': 'Backend Developer', 'location': 'Remote', 'salary': '90k'},
+  
+]
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=jobs)
 
 
 if __name__ == '__main__':
