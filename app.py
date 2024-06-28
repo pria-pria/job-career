f33e735a74b54f37c1c5dc6ebbda46e3bb14f16b
Priya Dugg <thepriyaduggal@gmail.com>
2024-06-28 23:25:41 +0000

    commit

--- app.py
+++ app.py
@@ -36,10 +36,5 @@ def hello_world():
   return render_template('home.html', jobs=jobs)
 
 
-@app.route('/api/jobs')
-def list_jobs():
-  return jsonify(jobs)
-
-
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
