c3b59800ca1be38af555b9674eff3ed9bdc1d6f4
Priya Dugg <thepriyaduggal@gmail.com>
2024-06-28 23:22:26 +0000

    commited by Priya

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-jobs =[
-  {'title': 'Python Developer', 'location': 'Remote', 'salary': '100k'},
-  {'title': 'Data Analyst', 'location': 'Remote', 'salary': '120k'},
-  {'title': 'Data Scientist', 'location': 'Remote', 'salary': '150k'},
-  {'title': 'Frontend Developer', 'location': 'Remote', 'salary': '80k'},
-  {'title': 'Backend Developer', 'location': 'Remote', 'salary': '90k'},
-  
+jobs = [
+    {
+        'title': 'Python Developer',
+        'location': 'Remote',
+        'salary': '100k'
+    },
+    {
+        'title': 'Data Analyst',
+        'location': 'Remote',
+        'salary': '120k'
+    },
+    {
+        'title': 'Data Scientist',
+        'location': 'Remote',
+        'salary': '150k'
+    },
+    {
+        'title': 'Frontend Developer',
+        'location': 'Remote',
+        'salary': '80k'
+    },
+    {
+        'title': 'Backend Developer',
+        'location': 'Remote',
+        'salary': '90k'
+    },
 ]
+
+
 @app.route('/')
 def hello_world():
   return render_template('home.html', jobs=jobs)
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
